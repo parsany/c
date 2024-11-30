4b4245075fa815e8e2172a7618f4d1333c65924f
Parsa <vvsparsa@gmail.com>
2024-11-30 17:47:52 +0330

    a

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -4,8 +4,8 @@ export default function main() {
     return (
         <div className={Styles.container}>
             <h1>Contact Me</h1>
-            <p>Email: <a href="mailto:parsaniavand0 -gmail.com">parsaniavand0 - gmail.com</a></p>
-            <p>Telegram: <a href="https://t.me/velvetphy">@velvetphy</a></p>
+            <p>Email: <a href="mailto:parsaniavand0 -gmail.com">parsaniavand0 at: gmail.com</a></p>
+            <p>Telegram: <a href="https://t.me/velvetphy">at:velvetphy</a></p>
             <p>LinkedIn: <a href="https://www.linkedin.com/in/parsany/">parsany</a></p>
             <p>Phone: +1234567890</p>
             <p>Address: 123 Main St, Anytown, USA</p>
