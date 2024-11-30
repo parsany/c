16f26d40fa213746700c3ee4f861219250d48bf9
Parsa <vvsparsa@gmail.com>
2024-11-30 17:45:24 +0330

    dd

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import styles from "@/styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <p ClassName={styles.page}>diddy</p>
+      <p ClassName={styles.page}> &lt;p&gt; diddy</p>
     </div>
   );
 }
