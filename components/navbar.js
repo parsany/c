6f0f27d0226c7b9e0bb58871db39c8e029a6ed8d
Parsa <vvsparsa@gmail.com>
2024-11-30 17:38:28 +0330

    in2

---
+++ components/navbar.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import Link from "next/link";
+import Styles from "@/styles/Navbar.module.css";
+
+export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <header className={Styles.header} role="banner">
+      <nav className={Styles.navbar} role="navigation">
+        <li>
+          <Link href="/" className={Styles.logo}>
+            <p>Home</p>
+          </Link>
+        </li>
+
+        <button
+          className={`${Styles.hamburger} ${menuOpen ? Styles.open : ""}`}
+          onClick={toggleMenu}
+          aria-label="Toggle navigation menu"
+        >
+          <span className={Styles.line}></span>
+          <span className={Styles.line}></span>
+          <span className={Styles.line}></span>
+        </button>
+
+        <ul
+          className={`${Styles.navlist} ${menuOpen ? Styles.navlistOpen : ""}`}
+        >
+          <li className={Styles.navitem}>
+            <Link href="/projects">
+              <p>Projects</p>
+            </Link>
+          </li>
+          <li className={Styles.navitem}>
+            <Link href="/experience">
+              <p>Experience</p>
+            </Link>
+          </li>
+          <li className={Styles.navitem}>
+            <Link href="/posts">
+              <p>Posts</p>
+            </Link>
+          </li>
+          <li className={Styles.navitem}>
+            <Link href="/materials">
+              <p>Materials</p>
+            </Link>
+          </li>
+          <li className={Styles.navitem}>
+            <Link href="/about">
+              <p>About</p>
+            </Link>
+          </li>
+          <li className={Styles.navitem}>
+            <Link href="/contact">
+              <p>Contact</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
