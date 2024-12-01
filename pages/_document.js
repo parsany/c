598274b0e8a8091065059359bc614c375b780e25
Parsa <vvsparsa@gmail.com>
2024-12-01 11:37:04 +0330

    fav

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>-_@-Under Maintenance-_$-</title>
+      <title>-_@-Under Maintenance-_$-</title>
       </Head>
       <body>
         <Main />
