36aba02a59e9fa54ab472ccc83115f6e2c4ddd83
Parsa <vvsparsa@gmail.com>
2024-12-01 11:59:36 +0330

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <title>-_@-Under Maintenance-_$-</title>
+      <link rel="shortcut icon" href="/c/favicon.ico" />
       </Head>
       <body>
         <Main />
