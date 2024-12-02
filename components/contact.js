03b7f034edb0f0a7130d4c807e99803f6d320425
Parsa <vvsparsa@gmail.com>
2024-12-02 04:47:13 +0330

    contactjs

--- components/contact.js
+++ components/contact.js
@@ -1,11 +1,13 @@
-import Styles from "@/styles/Contact.module.css";
+import Styles from "@/styles/Contact.white.module.css";
 
 export default function Contact() {
     return (
             <div className={Styles.container}>
                 <h1>Contact Me</h1>
                 <div className={Styles.contactInfo}>
-                    
+                    <p>Email: <a href="mailto:parsaniavand0 at gmail.com">parsaniavand0 at gmail.com</a></p>
+                    <p>Telegram: <a href="https://t.me/velvetphy">at velvetphy</a></p>
+                    <p>LinkedIn: <a href="https://www.linkedin.com/in/parsany/">linkedin.com/in/parsany</a></p>
                 </div>
         </div>
     );
