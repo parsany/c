9ba22192a75ffca163450fa1ef4f00e57e660c2e
Parsa <vvsparsa@gmail.com>
2024-12-07 14:07:16 +0330

    proj

--- components/projects.js
+++ components/projects.js
@@ -6,6 +6,14 @@ import { ProjectStuff } from "@/public/JSONJS";
 export default function Projects({ NumberShown }) {
   const [hoveredId, setHoveredId] = useState(null);
 
+  const handleMouseEnter = (id, index) => {
+    setHoveredId({ id, index });
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredId(null);
+  };
+
   return (
     <div className={styles.elements}>
       <h1 className={styles.title}>Selected Projects</h1>
@@ -16,14 +24,25 @@ export default function Projects({ NumberShown }) {
       <div className={styles.grid}>
         {ProjectStuff.sort((a, b) => new Date(b.date) - new Date(a.date))
           .slice(0, NumberShown === 0 ? undefined : NumberShown)
-          .map((project) => (
+          .map((project, index) => (
             <div
               key={project.id}
               className={`${styles.card} ${
-                hoveredId === project.id ? styles.active : styles.inactive
+                NumberShown === 3 && hoveredId?.id === project.id
+                  ? project.id === 1
+                    ? styles.selectedLeft 
+                    : project.id === 2
+                    ? styles.selected
+                    : project.id === 3
+                    ? styles.selectedRight
+                    : ""
+                  : ""
+              } ${
+                NumberShown === 3 && hoveredId &&
+                (hoveredId.index < index ? styles.moveRight : styles.moveLeft)
               }`}
-              onMouseEnter={() => setHoveredId(project.id)}
-              onMouseLeave={() => setHoveredId(null)}
+              onMouseEnter={() => handleMouseEnter(project.id, index)}
+              onMouseLeave={handleMouseLeave}
             >
               <a
                 href={project.link}
@@ -45,7 +64,6 @@ export default function Projects({ NumberShown }) {
                   <p className={styles.projectDescription}>
                     {project.description}
                   </p>
-                  {/* <p className={styles.projectDate}>{project.date}</p> */}
                 </div>
               </a>
             </div>
