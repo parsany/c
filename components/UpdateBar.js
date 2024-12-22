001d83b2e35da7016c0821669488aa2453f3d466
Parsa <vvsparsa@gmail.com>
2024-12-22 23:14:53 +0330

    navb

--- components/UpdateBar.js
+++ components/UpdateBar.js
@@ -1,9 +1,10 @@
 import styles from "@/styles/UpdateBar.module.css";
+import Link from "next/link";
 
 export default function UpdateBar() {
   return (
     <div className={styles.updateBar}>
-      <p>if you coming from the channel, the blog and materials will be up soon!</p>
+      <p>if you coming from the channel, the blog and <Link href="/materials" className={styles.link}>materials</Link> will be up soon!</p>
     </div>
   );
 }
