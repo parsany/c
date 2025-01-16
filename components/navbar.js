a36e256d623c1ab1cc8752de4d3d779c83239ce5
quantinity <vvsparsa@gmail.com>
2025-01-16 11:29:42 +0330

    mobile changes

--- components/navbar.js
+++ components/navbar.js
@@ -40,49 +40,66 @@ export default function NavBar() {
 
   return (
     <header className={Styles.header} role="banner">
-      <nav className={Styles.navbar} role="navigation">
-        <Link href="/" className={Styles.logo} onClick={handleLinkClick}>
-          <p>Phy</p>
-        </Link>
+  <nav className={Styles.navbar} role="navigation">
+    <Link href="/" className={Styles.logo} onClick={handleLinkClick}>
+      <p>Phy</p>
+    </Link>
 
-        <button
-          className={`${Styles.hamburger} ${menuOpen ? Styles.open : ""}`}
-          onClick={toggleMenu}
-          aria-label="Toggle navigation menu"
-        >
-          <span className={Styles.line}></span>
-          <span className={Styles.line}></span>
-          <span className={Styles.line}></span>
-        </button>
+    <div className={Styles.mobileControls}>
+      <a onClick={toggleTheme} className={Styles.toggleMode}>
+        <Image
+          src={
+            isDarkMode
+              ? "/c/icons/dark_mode.svg"
+              : "/c/icons/light_mode.svg"
+          }
+          alt="Toggle Icon"
+          className={Styles.togglecolor}
+          width={20}
+          height={20}
+        />
+      </a>
 
-        <ul
-          className={`${Styles.navlist} ${menuOpen ? Styles.navlistOpen : ""}`}
-        >
-          {NAV_LINKS.map(({ title, href }) => (
-            <li key={href} className={Styles.navitem}>
-              <Link href={href} onClick={handleLinkClick}>
-                <p>{title}</p>
-              </Link>
-            </li>
-          ))}
+      <button
+        className={`${Styles.hamburger} ${menuOpen ? Styles.open : ""}`}
+        onClick={toggleMenu}
+        aria-label="Toggle navigation menu"
+      >
+        <span className={Styles.line}></span>
+        <span className={Styles.line}></span>
+        <span className={Styles.line}></span>
+      </button>
+    </div>
 
-          <li className={Styles.navitem}>
-            <a onClick={toggleTheme} className={Styles.toggleMode}>
-              <Image
-                src={
-                  isDarkMode
-                    ? "/c/icons/dark_mode.svg"
-                    : "/c/icons/light_mode.svg"
-                }
-                alt="Toggle Icon"
-                className={Styles.togglecolor}
-                width={20}
-                height={20}
-              />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <ul
+      className={`${Styles.navlist} ${menuOpen ? Styles.navlistOpen : ""}`}
+    >
+      {NAV_LINKS.map(({ title, href }) => (
+        <li key={href} className={Styles.navitem}>
+          <Link href={href} onClick={handleLinkClick}>
+            <p>{title}</p>
+          </Link>
+        </li>
+      ))}
+
+      <li className={Styles.navitem}>
+        <a onClick={toggleTheme} className={Styles.toggleMode}>
+          <Image
+            src={
+              isDarkMode
+                ? "/c/icons/dark_mode.svg"
+                : "/c/icons/light_mode.svg"
+            }
+            alt="Toggle Icon"
+            className={Styles.togglecolor}
+            width={20}
+            height={20}
+          />
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
   );
 }
