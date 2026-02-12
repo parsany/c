1ed405b149f338b766c9bd9f03ab54b7825aa255
quantinity <vvsparsa@gmail.com>
2026-02-12 10:09:44 +0330

    VAE

--- components/UpdateBar.js
+++ components/UpdateBar.js
@@ -5,7 +5,7 @@ export default function UpdateBar() {
   return (
     <div className={styles.updateBar}>
       <p>2026 update: website havent been updated for so long, ill make another but wont be anytime soon</p>
-      <p>contact me for resume and current projects (i don&apos;t add them here)</p>
+      <p>contact me for resume and current projects (i don&apos;t add professional ones here)</p>
     </div>
   );
 }
