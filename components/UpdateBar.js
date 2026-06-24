8f2871297a8d7d912029ecb4f27107f29c4acb42
Parsa <vvsparsa@gmail.com>
2026-06-24 14:18:46 +0330

    update bar

--- components/UpdateBar.js
+++ components/UpdateBar.js
@@ -6,7 +6,7 @@ export default function UpdateBar() {
     <div className={styles.updateBar}>
       <p>
         <Link href="/projects/professional" className={styles.link}>
-          Explore my latest professional projects &rarr;
+          Explore my latest professional projects  (contact me for more) &rarr;
         </Link>
       </p>
     </div>
